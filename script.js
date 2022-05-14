fd58027c94811c768d932610dd1e4ffea0ab0b2f
Chijioke Chika <8pluscreative@gmail.com>
2022-05-14 10:35:21 -0400

    Update

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ let newCardBtn = document.getElementById('new-card')
 
 let firstCard = 9;
 let secondCard = 4;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let isAlive = true;
 
 let message = "";
 
-function startGame() {
+function renderGame() {
   if (sum <= 20) {
     message = "Do you want to draw a new Card?";
   } else if (sum === 21) {
@@ -22,13 +23,15 @@ function startGame() {
   }   
   messageEl.textContent = message;
 	sumEl += sumEl.textContent = "Sum:" + " " + sum;
-	cardEl.textContent = "Cards:" + " " + firstCard + " " + secondCard;
+	cardEl.textContent = "Cards:" + " " + cards[0] + " " + cards[1];
 }
 
 function newCard(){
-  console.log("Drawing new Card");
+  let card = 7;
+  sum += card;; 
+  renderGame();
 }
 
-startBtn.addEventListener("click", startGame)
+startBtn.addEventListener("click", renderGame)
 newCardBtn.addEventListener("click", newCard)
 
